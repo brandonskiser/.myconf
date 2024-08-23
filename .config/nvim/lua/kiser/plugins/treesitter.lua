d2f8700b2276ec564b994dfd13beb5f07adb6d7a
Brandon Kiser <brandonkns66@gmail.com>
2024-08-22 17:25:10 -0700

    Install nvim javascript treesitter thing

--- .config/nvim/lua/kiser/plugins/treesitter.lua
+++ .config/nvim/lua/kiser/plugins/treesitter.lua
@@ -17,7 +17,7 @@ return {
                 auto_install = true,
 
                 -- List of parsers to ignore installing (for "all")
-                ignore_install = { "javascript" },
+                -- ignore_install = { "javascript" },
 
                 highlight = {
                     -- `false` will disable the whole extension
