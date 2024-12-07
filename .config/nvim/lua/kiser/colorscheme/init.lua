dc36a387f6ae7ede7f5df8260d1ffc6e79c3aba8
Brandon Kiser <brandonkns66@gmail.com>
2024-12-06 18:44:09 -0800

    Colorscheme stuff kind of

--- .config/nvim/lua/kiser/colorscheme/init.lua
+++ .config/nvim/lua/kiser/colorscheme/init.lua
@@ -1,14 +1,3 @@
-local prefix = "kiser/colorscheme/"
-
--- require(prefix .. "sonokai")    -- Prob the best
--- require(prefix .. "tokyonight") -- Ehh not good, should delete
--- require(prefix .. "gruvbox")    -- idk, kinda trash tbh
--- require(prefix .. "catppuccin") -- All of this just hurts my eyes,
---                                    but might be good if I get transparent backgrounds working
--- require(prefix .. "onedark")    -- Good dark blue type scheme
--- require(prefix .. "vscode")
--- require(prefix .. "monokai-pro")
-
 Colorscheme = "cyberdream"
 
 if Colorscheme == nil then
--- .config/nvim/lua/kiser/plugins/colorscheme.lua
+++ .config/nvim/lua/kiser/plugins/colorscheme.lua
@@ -1,11 +1,19 @@
 return {
     {
+        "ellisonleao/gruvbox.nvim",
+        priority = 1000,
+        opts = {
+            bold = false,
+            contrast = "hard",
+        },
+    },
+    {
         "scottmckendry/cyberdream.nvim",
         lazy = false,
         priority = 1000,
         opts = {
             transparent = true,
-        }
+        },
     },
     {
         'loctvl842/monokai-pro.nvim',
@@ -55,9 +63,29 @@ return {
             ---@param c Colorscheme
             override = function(c) end,
         },
-        config = function(_, opts)
-            require('monokai-pro').setup(opts)
-            vim.cmd('colorscheme monokai-pro')
+    },
+
+    -- TODO: implement my own base16 variant
+    {
+        "wincent/base16-nvim",
+        lazy = false,    -- load at start
+        priority = 1000, -- load first
+        config = function()
+            -- vim.cmd([[colorscheme base16-gruvbox-dark-hard]])
+            -- vim.o.background = 'dark'
+            -- XXX: hi Normal ctermbg=NONE
+            -- Make comments more prominent -- they are important.
+            -- local bools = vim.api.nvim_get_hl(0, { name = 'Boolean' })
+            -- vim.api.nvim_set_hl(0, 'Comment', bools)
+            -- Make it clearly visible which argument we're at.
+            -- local marked = vim.api.nvim_get_hl(0, { name = 'PMenu' })
+            -- vim.api.nvim_set_hl(0, 'LspSignatureActiveParameter',
+            --     { fg = marked.fg, bg = marked.bg, ctermfg = marked.ctermfg, ctermbg = marked.ctermbg, bold = true })
+            -- XXX
+            -- Would be nice to customize the highlighting of warnings and the like to make
+            -- them less glaring. But alas
+            -- https://github.com/nvim-lua/lsp_extensions.nvim/issues/21
+            -- call Base16hi("CocHintSign", g:base16_gui03, "", g:base16_cterm03, "", "", "")
         end
-    }
+    },
 }
