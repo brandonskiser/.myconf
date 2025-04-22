b61581964b5accfb19a5750b618a711a98f72953
Brandon Kiser <brandonkns66@gmail.com>
2025-04-22 13:37:39 -0700

    Fix broken rust lsp

--- .config/nvim/lua/kiser/plugins/lsp/rust.lua
+++ .config/nvim/lua/kiser/plugins/lsp/rust.lua
@@ -7,6 +7,16 @@ vim.g.rustaceanvim = {
         on_attach = function(_, bufnr)
             -- Add keymaps here.
             vim.keymap.set("n", "K", ":RustLsp hover actions<CR>", { noremap = true, silent = true, buffer = bufnr })
+            for _, method in ipairs({ "textDocument/diagnostic", "workspace/diagnostic" }) do
+                -- https://github.com/neovim/neovim/issues/30985#issuecomment-2447329525
+                local default_diagnostic_handler = vim.lsp.handlers[method]
+                vim.lsp.handlers[method] = function(err, result, context, config)
+                    if err ~= nil and err.code == -32802 then
+                        return
+                    end
+                    return default_diagnostic_handler(err, result, context, config)
+                end
+            end
         end,
         settings = {
             ["rust-analyzer"] = {
